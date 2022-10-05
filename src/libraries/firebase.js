dde836e7dcc13fb2f82b03b33c9349bbd91520f2
Fiona Lazarus <72126177+FionaL7@users.noreply.github.com>
2022-10-05 15:51:22 +0530

    Update firebase.js

--- src/libraries/firebase.js
+++ src/libraries/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyB3P1gLaQoecWE2Tllma_cLSUgXspShO2M",
+  authDomain: "instagram-7a0eb.firebaseapp.com",
+  projectId: "instagram-7a0eb",
+  storageBucket: "instagram-7a0eb.appspot.com",
+  messagingSenderId: "482361647733",
+  appId: "1:482361647733:web:f47df9144657257004ce3c",
 };
 
 const app = initializeApp(config);
